b88eac664a55ab5887043ce8d88e04a8a58a04b9
Songchaohui <992414441@qq.com>
2017-10-24 18:11:04 +0800

    '2017年10月24日;

---
+++ js/login.js
@@ -0,0 +1,75 @@
+$(function(){
+    var host = 'http://' + document.domain;
+    var allCookiie = document.cookie;
+    if (allCookiie.indexOf("username")!=-1 && allCookiie.indexOf("user_id")){
+        window.location.href = host+"/WorkDailySys/_index.php";
+    }else{
+        var loginBox = new Vue({
+            el:'#login-box',
+            data:{
+                email:'',
+                pass:''
+            },
+            methods:{
+                login:function () {
+                    var _self = this;
+                    $.ajax({
+                        url:host +'/workDailySys/user/login.php',
+                        type:'POST',
+                        dataType:'json',
+                        data:{email:_self.email,pass:_self.pass},
+                        success:function(response){
+                            console.log(response);
+                            if(response.status){
+                                let modalBodyText = [];
+                                $.each(response.msg,function(index,item){
+                                    modalBodyText.push({text:item});
+                                });
+                                siteModel.items = modalBodyText;
+                                $('#siteModal').modal('show');
+                                setTimeout(function(){
+                                    $('#siteModal').modal('hide');
+                                    window.location.href = host+"/workDailySys/user-agent/_index.php";
+                                },1000);
+                            }else{
+                                let modalBodyText = [];
+                                $.each(response.errors,function(index,item){
+                                    modalBodyText.push({text:item});
+                                });
+                                siteModel.items = modalBodyText;
+                                $('#siteModal').modal('show');
+                                setTimeout(function(){
+                                    $('#siteModal').modal('hide');
+                                },2000);
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
+        var siteModel = new Vue({
+            el:'#siteModal',
+            data:{
+                items:[],
+                modalHeader:"登录状态",
+                btn1:{
+                    text:"关闭",
+                    show:true,
+                    classList:{
+                        'btn-default':true,
+                        'btn-primary':false
+                    }
+                },
+                btn2:{
+                    text:"确定",
+                    show:false,
+                    classList:{
+                        'btn-default':true,
+                        'btn-primary':false
+                    }
+                }
+            }
+        });
+    }
+});
---
+++ user-agent/_index.php
@@ -0,0 +1,29 @@
+<?php
+$site_title="工作日报小demo-首页";
+$page_css_list = array(
+  "../css/mainpage.css"
+);
+include '../public/site-header.php';
+?>
+<div class="container recentDaily">
+    <div class="row block-title">
+        <h2>工作汇报列表</h2>
+    </div>
+    <div class="row block-content">
+        <div class="col-md-4">
+            <h3><span>1</span>Title</h3>
+            <p>使用谷歌浏览器调js问题，发现谷歌浏览器总是缓存之前的js信息，就想禁止浏览器缓存，在网络上找来找去都是之前的版本的谷歌浏览器的设置方法，我的浏览器版本是55.0.2883.87 m的和之前的不同了，后经过摸索找到了方法，如下</p>
+        </div>
+        <div class="col-md-4">
+            <h3><span>2</span>Title</h3>
+            <p>使用谷歌浏览器调js问题，发现谷歌浏览器总是缓存之前的js信息，就想禁止浏览器缓存，在网络上找来找去都是之前的版本的谷歌浏览器的设置方法，我的浏览器版本是55.0.2883.87 m的和之前的不同了，后经过摸索找到了方法，如下</p>
+        </div>
+        <div class="col-md-4">
+            <h3><span>3</span>Title</h3>
+            <p>使用谷歌浏览器调js问题，发现谷歌浏览器总是缓存之前的js信息，就想禁止浏览器缓存，在网络上找来找去都是之前的版本的谷歌浏览器的设置方法，我的浏览器版本是55.0.2883.87 m的和之前的不同了，后经过摸索找到了方法，如下</p>
+        </div>
+    </div>
+</div>
+<?php
+include '../public/site-footer.php';
+?>
